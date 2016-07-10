99f2f38e6161680ef09ec4bd0515d6a0cdcfac50
Thom Linton <tlinton@pdx.edu>
2016-07-10 02:37:50 -0700

    Updates dependencies to target Elasticsearch 2.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 setup(
     name='DjangoInelasticModels',
     version='1.1',
-    install_requires=['elasticsearch>=1.0.0,<2.0.0','elasticsearch-dsl<2.0.0'],
+    install_requires=['elasticsearch>=2.0.0,<5.0.0','elasticsearch-dsl>=2.0.0,<5.0.0'],
     packages=find_packages(),
     long_description=open('README.rst').read(),
     author='Thom Linton',
