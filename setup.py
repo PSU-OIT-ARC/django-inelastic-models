1bd48ea31463828d577681f8bc84abbae43545f0
Thom Linton <tlinton@pdx.edu>
2016-07-12 15:10:24 -0700

    Updates setuptools definition.

--- setup.py
+++ setup.py
@@ -4,10 +4,14 @@ from setuptools import find_packages, setup
 setup(
     name='DjangoInelasticModels',
     version='1.2',
-    install_requires=['elasticsearch>=2.0.0,<5.0.0','elasticsearch-dsl>=2.0.0,<5.0.0'],
-    packages=find_packages(),
-    long_description=open('README.rst').read(),
     author='Thom Linton',
+    author_email='tlinton@pdx.edu',
+    url='https://github.com/PSU-OIT-ARC/django-inelastic-models',
+    long_description=open('README.rst').read(),
+
+    packages=find_packages(),
+    install_requires=['elasticsearch>=2.0.0,<5.0.0',
+                      'elasticsearch-dsl>=2.0.0,<5.0.0'],
     extras_require={
         'test': ["django" + ("<1.7" if sys.version_info[:2] < (2, 7) else ""),
                  'django-dynamic-fixture'],
