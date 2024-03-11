005ccaa99f914c2fbb1235a4c0e3386158e7572d
Thom Linton <tlinton@pdx.edu>
2024-03-11 15:16:09 -0700

    Bumps version to 8.0.0.dev1.

--- inelastic_models/__init__.py
+++ inelastic_models/__init__.py
@@ -1,6 +1,6 @@
 """Declarative Elasticsearch indexes for Django models."""
 
 default_app_config = 'inelastic_models.apps.SearchConfig'
-__version__ = '8.0.0.dev0'
+__version__ = '8.0.0.dev1'
 
 __all__ = ['__version__']
