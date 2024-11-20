b995c4008886f384ef4d3b36332b323bab777afb
Thom Linton <tlinton@pdx.edu>
2024-11-19 20:51:29 -0800

    Removes unnecessary app config configuration.

--- inelastic_models/__init__.py
+++ inelastic_models/__init__.py
@@ -1,6 +1,5 @@
 """Declarative Elasticsearch indexes for Django models."""
 
-default_app_config = 'inelastic_models.apps.SearchConfig'
 __version__ = '8.0.0.dev2'
 
 __all__ = ['__version__']
--- inelastic_models/apps.py
+++ inelastic_models/apps.py
@@ -6,7 +6,11 @@ from inelastic_models.utils import autoload_submodules
 class SearchConfig(AppConfig):
     name = 'inelastic_models'
     verbose_name = "Search"
+    default = True
 
     def ready(self):
+        # ensure 'indexes' submodules are loaded
         autoload_submodules(['indexes'])
+
+        # ensure signal handlers are loaded/registered
         from . import receivers
