PSU-OIT-ARC/django-inelastic-models

commit f616ebd8b9c5554f92b2a9e3be2cc10477138d6b

Delegates queryset construction to 'Search.get_base_qs'.
Refs: 3299f6b

inelastic_models/indexes.py
@@ -212,14 +212,16 @@ class Search(FieldMappingMixin):
                 )
             elif hasattr(field, "model") and hasattr(field.model, "_search_meta"):
                 if isinstance(instance_value, dict):
-                    _instance = field.model.objects.get(pk=instance_value.get("pk"))
+                    _queryset = field.model._search_meta().get_base_qs()
+                    _instance = _queryset.get(pk=instance_value.get("pk"))
                     if field.model._search_meta().has_index_changed(
                             _instance, fields=field.get_fields().keys()
                     ):
                         return True
                 elif isinstance(instance_value, list):
                     for _el in instance_value:
-                        _instance = field.model.objects.get(pk=_el.get("pk"))
+                        queryset = field.model._search_meta().get_base_qs()
+                        _instance = _queryset.get(pk=_el.get("pk"))
                         if field.model._search_meta().has_index_changed(
                                 _instance, fields=field.get_fields().keys()
                         ):

commit 972aa8cac0472ca7f4c7e20e490731024a730ef9

Adds additional guards to 'has_index_changed'.
In cases where the examined field is attached to a model which is
bound to a 'Search' instance, additional checks are performed to
ensure that:

- the given instance value (either dict or list) are not None
- the instance(s) given by primary keys fetched from the index
  represent entries which may (still) be accessed via the ORM.

Fixes: f616ebd
Refs: 979f515

inelastic_models/indexes.py
@@ -211,19 +211,27 @@ class Search(FieldMappingMixin):
                     )
                 )
             elif hasattr(field, "model") and hasattr(field.model, "_search_meta"):
-                if isinstance(instance_value, dict):
+                if isinstance(instance_value, dict) and instance_value:
                     _queryset = field.model._search_meta().get_base_qs()
-                    _instance = _queryset.get(pk=instance_value.get("pk"))
-                    if field.model._search_meta().has_index_changed(
-                            _instance, fields=field.get_fields().keys()
+                    _maybe_instance = _queryset.filter(pk=instance_value.get("pk"))
+                    if (
+                            _maybe_instance.exists() and
+                            field.model._search_meta().has_index_changed(
+                                _maybe_instance.get(),
+                                fields=field.get_fields().keys()
+                            )
                     ):
                         return True
-                elif isinstance(instance_value, list):
+                elif isinstance(instance_value, list) and instance_value:
                     for _el in instance_value:
-                        queryset = field.model._search_meta().get_base_qs()
-                        _instance = _queryset.get(pk=_el.get("pk"))
-                        if field.model._search_meta().has_index_changed(
-                                _instance, fields=field.get_fields().keys()
+                        _queryset = field.model._search_meta().get_base_qs()
+                        _maybe_instance = _queryset.filter(pk=_el.get("pk"))
+                        if (
+                                _maybe_instance.exists() and
+                                field.model._search_meta().has_index_changed(
+                                    _maybe_instance.get(),
+                                    fields=field.get_fields().keys()
+                                )
                         ):
                             return True
             elif index_value != instance_value:
@@ -405,6 +413,7 @@ class Search(FieldMappingMixin):
 
         qs = qs.filter(**filters)
 
+        # TODO: is this change acceptable?
         if self.index_ordering is not None:
             qs = qs.order_by(*self.index_ordering)
 