PSU-OIT-ARC/django-inelastic-models

commit 93e4cf65fe28c029aae31c421dfa0d3265f9a863

Adds proper, functional implementation of 'handle_m2m'.

inelastic_models/receivers.py
@@ -131,11 +131,60 @@ def handle_m2m(sender, **kwargs):
     """
     TBD
     """
-    if kwargs['action'].startswith("pre_"):
-        (model_cls, instance) = (kwargs.get('model'), kwargs.get('instance'))
+    (model_cls, instance, reverse) = (
+        kwargs.get('model'), kwargs.get('instance'), kwargs.get('reverse')
+    )
+
+    if kwargs['action'] == "pre_clear":
+        field = None
+        for _field in sender._meta.get_fields():
+            if _field.related_model is None:
+                continue
+            if issubclass(type(instance), _field.related_model):
+                field = _field
+
+        objs = []
+        if field is not None:
+            objs = model_cls.objects.filter(**{field.name: instance.pk})
+        if getattr(instance, "_inelasticmodels_m2m_dependents", None) is None:
+            instance._inelasticmodels_m2m_dependents = {}
+
+        pk_set = set(objs.values_list('pk', flat=True))
+        if instance._inelasticmodels_m2m_dependents.get(model_cls):
+            current = instance._inelasticmodels_m2m_dependents[model_cls]
+            pk_set = current | pk_set
+
+        instance._inelasticmodels_m2m_dependents = {model_cls: pk_set}
+
+    elif kwargs['action'] == "post_clear":
+        logger.debug("M2M dependents of 'clear' on {}".format(instance))
+        logger.debug("- {}".format(instance._inelasticmodels_m2m_dependents))
+
+        if reverse:
+            dependents = instance._inelasticmodels_m2m_dependents.pop(model_cls)
+            for pk in dependents:
+                dependent = model_cls.objects.get(pk=pk)
+                update_search_index(sender, instance=dependent)
+        else:
+            update_search_index(sender, instance=instance)
+
+    elif kwargs['action'].startswith("pre_"):
+        queryset = model_cls.objects.filter(pk__in=kwargs.get("pk_set"))
         instance._inelasticmodels_m2m_dependents = {
-            model_cls: model_cls.objects.filter(pk__in=kwargs.get("pk_set"))
+            model_cls: set(queryset.values_list('pk', flat=True))
         }
+
+    elif kwargs['action'].startswith("post_"):
+        logger.debug("M2M dependents of 'add/remove' on {}".format(instance))
+        logger.debug("- {}".format(instance._inelasticmodels_m2m_dependents))
+
+        if reverse:
+            dependents = instance._inelasticmodels_m2m_dependents.pop(model_cls)
+            for pk in dependents:
+                dependent = model_cls.objects.get(pk=pk)
+                update_search_index(sender, instance=dependent)
+        else:
+            update_search_index(sender, instance=instance)
 
 
 @receiver(signals.post_delete)

commit f5a907ecc658d0a3b43c344a06e5a82b8d724ff9

Revises implementation of 'should_index' helper.
This function now also evaluates to True when given the keyword
argument 'signal' with is equal to 'm2m_changed'.

Additionally, ensures that the given 'sender' to 'update_search_index'
represents the instance type and not the caller of 'm2m_changed'
handled in 'handle_m2m'.

Refs: 149cf6d
Refs: e968730
Refs: 1658769

Fixes: 93e4cf6

inelastic_models/receivers.py
@@ -84,9 +84,9 @@ def should_index(sender, instance, signal=None):
     if sender not in get_search_models():
         return False
 
-    if signal is not None and signal == signals.post_delete:
+    if signal is not None and signal in [signals.m2m_changed, signals.post_delete]:
         logger.debug(
-            "Marking captured deletion of '{}' as should_index.".format(instance)
+            "Marking captured m2m/deletion of '{}' as should_index.".format(instance)
         )
         return True
 
@@ -173,9 +173,11 @@ def handle_m2m(sender, **kwargs):
             dependents = instance._inelasticmodels_m2m_dependents.pop(model_cls)
             for pk in dependents:
                 dependent = model_cls.objects.get(pk=pk)
-                update_search_index(sender, instance=dependent)
+                update_search_index(
+                    model_cls, instance=dependent, signal=kwargs['signal']
+                )
         else:
-            update_search_index(sender, instance=instance)
+            update_search_index(model_cls, instance=instance, signal=kwargs['signal'])
 
     elif m2m_action.startswith("pre_"):
         queryset = model_cls.objects.filter(pk__in=kwargs.get("pk_set"))
@@ -191,9 +193,11 @@ def handle_m2m(sender, **kwargs):
             dependents = instance._inelasticmodels_m2m_dependents.pop(model_cls)
             for pk in dependents:
                 dependent = model_cls.objects.get(pk=pk)
-                update_search_index(sender, instance=dependent)
+                update_search_index(
+                    model_cls, instance=dependent, signal=kwargs['signal']
+                )
         else:
-            update_search_index(sender, instance=instance)
+            update_search_index(model_cls, instance=instance, signal=kwargs['signal'])
 
 
 @receiver(signals.post_delete)